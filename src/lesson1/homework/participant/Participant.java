27794437882023a9a5ffd1d424e44f1e8a5d7682
okrylov <krylov92oa@gmail.com>
2019-10-07 20:39:34 +0300

    Lesson 1 - homework

---
+++ src/lesson1/homework/participant/Participant.java
@@ -0,0 +1,14 @@
+package lesson1.homework.participant;
+
+import lesson1.homework.obstacle.Obstacle;
+
+public interface Participant {
+
+    int run();
+
+    int jump();
+
+    default boolean doAction(Obstacle obstacle) {
+        return obstacle.passObstacleBy(this);
+    };
+}
